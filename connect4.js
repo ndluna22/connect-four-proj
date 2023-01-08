921beb393a6589fafb78da71eede1b5a344842cb
ndluna22 <119833755+ndluna22@users.noreply.github.com>
2023-01-08 16:15:14 -0600

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -11,10 +11,10 @@ const HEIGHT = 6;
 let  currPlayer = 1; // active player: 1 or 2
 let  board = []; // array of rows, each row is array of cells  (board[y][x])
 
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
 
+
+//To create in-JS board structure below:    
+      
 function makeBoard() {
   // set "board" to empty HEIGHT x WIDTH matrix array
 
